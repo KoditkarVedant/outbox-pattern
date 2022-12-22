42204aaa0117bc2bb43fceb5a42b0110de5ff7e8
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-12-22 23:01:00 +0530

    Add message repository implementation

---
+++ Outbox.Infrastructure/MessageRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Outbox.Core;
+
+namespace Outbox.Infrastructure;
+
+public class MessageRepository : IMessageRepository
+{
+    private readonly DbContext _context;
+
+    public MessageRepository(OutboxContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Message?> GetMessageToDeliver(Partition partition)
+    {
+        var messages = _context.Set<Message>().OrderBy(x => x.ReceivedDateTime);
+            
+        var message = await messages.FirstOrDefaultAsync(
+            x => x.PartitionKey == partition.PartitionKey &&
+                 (x.DeliveryStatus != MessageDeliveryStatus.Sent && x.DeliveryStatus != MessageDeliveryStatus.Failed)
+        );
+
+        return message;
+    }
+
+    public async Task UpdateMessage(Message message)
+    {
+        _context.Set<Message>().Update(message);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddMessageToDeliver(Message message)
+    {
+        var partitionExists = await _context.Set<Partition>().AnyAsync(x => x.PartitionKey == message.PartitionKey);
+        if (!partitionExists)
+        {
+            await _context.Set<Partition>().AddAsync(new Partition()
+            {
+                PartitionKey = message.PartitionKey
+            });
+        }
+
+        await _context.Set<Message>().AddAsync(message);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Partition>> GetPartitions()
+    {
+        return await _context.Set<Partition>().ToListAsync();
+    }
+}
