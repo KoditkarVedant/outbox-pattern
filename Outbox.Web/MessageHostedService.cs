173bb068a1e32a37be33f36fe2193c9b1f46d687
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-12-22 23:14:44 +0530

    Add message hosted service

---
+++ Outbox.Web/MessageHostedService.cs
@@ -0,0 +1,64 @@
+﻿using Outbox.Core;
+
+namespace Outbox.Web;
+
+public class MessageHostedService : BackgroundService
+{
+    private readonly ILogger<MessageHostedService> _logger;
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly IHostApplicationLifetime _lifetime;
+    private readonly PeriodicTimer _periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+
+    public MessageHostedService(
+        ILogger<MessageHostedService> logger,
+        IServiceScopeFactory scopeFactory,
+        IHostApplicationLifetime lifetime)
+    {
+        _logger = logger;
+        _scopeFactory = scopeFactory;
+        _lifetime = lifetime;
+    }
+
+    private static async Task<bool> WaitForAppToStart(IHostApplicationLifetime lifetime,
+        CancellationToken stoppingToken)
+    {
+        var appStartedSource = new TaskCompletionSource();
+        var hostCancelledSource = new TaskCompletionSource();
+
+        await using var cancellationTokenRegistration =
+            lifetime.ApplicationStarted.Register(() => appStartedSource.SetResult());
+        await using var tokenRegistration = stoppingToken.Register(() => hostCancelledSource.SetResult());
+
+        var completedTask = await Task.WhenAny(
+            appStartedSource.Task,
+            hostCancelledSource.Task
+        ).ConfigureAwait(false);
+
+        return completedTask == appStartedSource.Task;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        if (!await WaitForAppToStart(_lifetime, stoppingToken))
+        {
+            return;
+        }
+
+        while (!stoppingToken.IsCancellationRequested && await _periodicTimer.WaitForNextTickAsync(stoppingToken))
+        {
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var messagePublisherService = scope.ServiceProvider.GetRequiredService<IMessagePublisherService>();
+                await messagePublisherService.StartAsync(stoppingToken);
+
+                _logger.LogInformation("MessagePublisherService has started...");
+                break;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error starting MessagePublisherService...");
+            }
+        }
+    }
+}
--- Outbox.Web/Program.cs
+++ Outbox.Web/Program.cs
@@ -1,9 +1,50 @@
+using System.Text.Json;
+using Outbox.Core;
+using Outbox.Infrastructure;
+using Outbox.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole();
+
+builder.Services.AddDbContext<OutboxContext>();
+
+builder.Services.AddSingleton<IPeriodicMessagePublisherStore, PeriodicMessagePublisherStore>();
+builder.Services.AddSingleton<IMessagePublisherService, MessagePublisherService>();
+builder.Services.AddSingleton<IMessageLocker, DistributedMessageLocker>();
+
+builder.Services.AddTransient<IMessagePublisher, MessagePublisher>();
+builder.Services.AddTransient<IMessageRepository, MessageRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "CacheInstance";
+});
+
+builder.Services.AddHostedService<MessageHostedService>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Running...");
 
+app.MapPost("/publish-messages", (ILogger<Program> logger,
+    IList<PublishMessage> messages,
+    IMessageRepository repository) =>
+{
+    foreach (var message in messages)
+    {
+        logger.LogInformation("Saving message in Partition {PartitionKey}", message.PartitionKey);
+        repository.AddMessageToDeliver(new Message()
+        {
+            PartitionKey = message.PartitionKey,
+            Content = JsonSerializer.Serialize(message.Content),
+            ReceivedDateTime = DateTime.UtcNow
+        });
+        logger.LogInformation("Saved message in Partition {PartitionKey}", message.PartitionKey);
+    }
+});
+
 app.Run();
+
+public record PublishMessage(string PartitionKey, object Content);
