d8f874dc7ed45d2fb7df9f98743f6a93df0dfae8
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-12-22 22:52:17 +0530

    Add Outbox DbContext & initial create migrations

---
+++ Outbox.Infrastructure/Migrations/20221221200338_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Outbox.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PartitionKey = table.Column<string>(type: "TEXT", nullable: true),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    DeliveryStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    RetryCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReceivedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PublishedDateTime = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partition",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PartitionKey = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partition", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partition_PartitionKey",
+                table: "Partition",
+                column: "PartitionKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+
+            migrationBuilder.DropTable(
+                name: "Partition");
+        }
+    }
+}
---
+++ Outbox.Infrastructure/OutboxContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Outbox.Core;
+
+namespace Outbox.Infrastructure;
+
+public class OutboxContext : DbContext
+{
+    private string DbPath { get; }
+    
+    public OutboxContext()
+    {
+        const Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "outbox.db");
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MessageTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new PartitionTypeConfiguration());
+    }
+}
+
+public class MessageTypeConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.PartitionKey);
+        builder.Property(x => x.Content).IsRequired();
+        builder.Property(x => x.DeliveryStatus);
+        builder.Property(x => x.RetryCount);
+        builder.Property(x => x.ReceivedDateTime);
+        builder.Property(x => x.PublishedDateTime);
+    }
+}
+
+public class PartitionTypeConfiguration : IEntityTypeConfiguration<Partition>
+{
+    public void Configure(EntityTypeBuilder<Partition> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.PartitionKey).IsUnique();
+    }
+}
