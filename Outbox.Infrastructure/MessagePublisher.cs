ab82e9f6f52d1bdb9971725768f8eaa5f8bb3c01
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-12-22 23:13:22 +0530

    Add message publisher

---
+++ Outbox.Infrastructure/MessagePublisher.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Outbox.Core;
+
+namespace Outbox.Infrastructure;
+
+public class MessagePublisher : IMessagePublisher
+{
+    private readonly ILogger<MessagePublisher> _logger;
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMessageLocker _messageLocker;
+
+    private static readonly JsonSerializerOptions Settings = new() { WriteIndented = true };
+
+    public MessagePublisher(
+        ILogger<MessagePublisher> logger,
+        IMessageRepository messageRepository,
+        IMessageLocker messageLocker)
+    {
+        _logger = logger;
+        _messageRepository = messageRepository;
+        _messageLocker = messageLocker;
+    }
+
+    public async Task PublishPendingMessages(Partition partition)
+    {
+        while (true)
+        {
+            var message = await _messageRepository.GetMessageToDeliver(partition);
+
+            if (message is null)
+            {
+                _logger.LogInformation(
+                    "No pending messages to deliver for partition {PartitionKey}",
+                    partition.PartitionKey
+                );
+
+                break;
+            }
+
+            var lockAcquired = await _messageLocker.LockMessage(message);
+            if (lockAcquired)
+            {
+                _logger.LogWarning(
+                    "Acquired lock on message \n{MessageJson}",
+                    JsonSerializer.Serialize(message, Settings)
+                );
+
+                message.DeliveryStatus = MessageDeliveryStatus.Picked;
+                await _messageRepository.UpdateMessage(message);
+                _logger.LogWarning("Message Picked \n{MessageJson}", JsonSerializer.Serialize(message, Settings));
+
+                // TODO: Add PublisherClient
+
+                message.DeliveryStatus = MessageDeliveryStatus.Sent;
+                message.PublishedDateTime = DateTime.UtcNow;
+                await _messageRepository.UpdateMessage(message);
+                _logger.LogWarning("Message Sent \n{MessageJson}", JsonSerializer.Serialize(message, Settings));
+            }
+            else
+            {
+                _logger.LogWarning(
+                    "Unable to acquire lock on message {MessageJson}",
+                    JsonSerializer.Serialize(message, Settings)
+                );
+            }
+        }
+    }
+}
---
+++ Outbox.Infrastructure/MessagePublisherService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Outbox.Core;
+
+namespace Outbox.Infrastructure;
+
+public class MessagePublisherService : PeriodicTask<MessagePublisherService>, IMessagePublisherService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public MessagePublisherService(ILogger<MessagePublisherService> logger, IServiceProvider serviceProvider)
+        : base(logger)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task Execute(CancellationToken cancellationToken = default)
+    {
+        var scope = _serviceProvider.CreateScope();
+        var messageRepository = scope.ServiceProvider.GetRequiredService<IMessageRepository>();
+        var periodicMessagePublisherStore =
+            scope.ServiceProvider.GetRequiredService<IPeriodicMessagePublisherStore>();
+
+        var partitions = await messageRepository.GetPartitions();
+
+        foreach (var partition in partitions)
+        {
+            // TODO: just set to this as non nullable field
+            var partitionKey = partition.PartitionKey ?? string.Empty;
+            if (periodicMessagePublisherStore.Exists(partitionKey))
+            {
+                Logger.LogInformation("PeriodicMessagePublisher already registered for Partition {PartitionKey}",
+                    partition.PartitionKey
+                );
+                continue;
+            }
+
+            var publisher = new PeriodicMessagePublisher(
+                partition,
+                _serviceProvider.GetRequiredService<ILogger<PeriodicMessagePublisher>>(),
+                _serviceProvider
+            );
+            periodicMessagePublisherStore.Add(partitionKey, publisher);
+            _ = publisher.StartAsync(cancellationToken);
+
+            Logger.LogInformation("Registered Periodic Message Publisher for Partition {PartitionKey}",
+                partition.PartitionKey
+            );
+        }
+    }
+
+    public override TimeSpan IntervalBetweenExecutions { get; } = TimeSpan.FromSeconds(15);
+}
